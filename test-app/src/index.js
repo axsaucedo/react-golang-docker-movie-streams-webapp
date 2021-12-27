09b02278ca7a1cfc517a483c55b4736bbf09363c
Alejandro Saucedo <axsauze@gmail.com>
2021-12-27 07:40:10 +0000

    initial hello world

--- test-app/src/index.js
+++ test-app/src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello world");
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+    render() {
+        return <div>
+            <h1>Hello, world!</h1>
+        </div>;
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
